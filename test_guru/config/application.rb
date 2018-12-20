03052a047dce1911ec4350290dcddf8d2fe34d83
Kseniya82 <chirkovaksenia82@gmail.com>
2018-12-20 15:33:51 +0300

    Add static page revision 1

--- test_guru/config/application.rb
+++ test_guru/config/application.rb
@@ -11,10 +11,9 @@ module TestGuru
 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.time_zone = 'Europe/Moscow'
+    config.time_zone = 'St. Petersburg'
     config.location = 'Saint-Peterburg'
     config.lang ='ru'
-    Version = '0.1.0'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
