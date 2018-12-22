e9a4e530af1c568afc9376ddce50aa8575559346
Kseniya82 <chirkovaksenia82@gmail.com>
2018-12-22 15:21:44 +0300

    Add models and migration

---
+++ test_guru/db/migrate/20181222112437_add_tables_columns_null_constraint.rb
@@ -0,0 +1,11 @@
+class AddTablesColumnsNullConstraint < ActiveRecord::Migration[5.2]
+  def change
+    change_column_null(:tests, :title, false)
+    change_column_null(:tests, :level, false)
+    change_column_null(:questions, :body, false)
+    change_column_null(:users, :name, false)
+    change_column_null(:users, :access, false)
+    change_column_null(:answers, :body, false)
+    change_column_null(:categories, :title, false)
+  end
+end
