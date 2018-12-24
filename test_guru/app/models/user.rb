d2413549e872377625c837223405565ad5205d8c
Kseniya82 <chirkovaksenia82@gmail.com>
2018-12-24 20:46:51 +0300

    Add ActiveRecord Queries revision 2

--- test_guru/app/models/user.rb
+++ test_guru/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   def select_all_tests(level)
     Test.joins('JOIN testers ON tests.id = testers.test_id')
-    .where(testers: { user_id: self.id }, tests: { level: level })
+    .where(level: level, testers: { user_id: self.id } )
   end
 end
--- test_guru/db/seeds.rb
+++ test_guru/db/seeds.rb
@@ -6,39 +6,45 @@
 #   movies = Movie.create!([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create!(name: 'Luke', movie: movies.first)
 
-category = []
-category << Category.create!(title: 'Easy')
-category << Category.create!(title: 'Middle')
-category << Category.create!(title: 'Hard')
+categories= Category.create!([
+  { title: 'Easy' },
+  { title: 'Middle' },
+  { title: 'Hard'}
+])
 
-test = []
-test << Test.create!(title: 'Физика', category_id: category[0].id)
-test << Test.create!(title: 'История', category_id: category[0].id)
-test << Test.create!(title: 'Философия', category_id: category[1].id)
-test << Test.create!(title: 'Искусство', category_id: category[2].id)
+tests = Test.create!([
+  { title: 'Физика', category_id: categories[0].id },
+  { title: 'История', category_id: categories[0].id },
+  { title: 'Философия', category_id: categories[1].id },
+  { title: 'Искусство', category_id: categories[2].id }
+])
 
-user = []
-user << User.create!(name:'Иван Иванович', access: 'user')
-user << User.create!(name:'Мария Петрова', access: 'user')
-user << User.create!(name:'Колобок', access: 'user')
+users = User.create!([
+  { name:'Иван Иванович', access: 'user' },
+  { name:'Мария Петрова', access: 'user' },
+  { name:'Колобок', access: 'user' }
+])
 
-question = []
-question << Question.create!(body: 'Какая физическая величнина измеряется в амперах', test_id: test[0].id)
-question << Question.create!(body: 'В каком году началась Вторая мировая война', test_id: test[1].id)
-question << Question.create!(body: 'Коиу принадлежит фраза: я мыслю, значит я существую', test_id: test[2].id)
-question << Question.create!(body: 'Картины Айвазовского', test_id: test[3].id)
+questions = Question.create!([
+  { body: 'Какая физическая величнина измеряется в амперах', test_id: tests[0].id },
+  { body: 'В каком году началась Вторая мировая война', test_id: tests[1].id },
+  { body: 'Коиу принадлежит фраза: я мыслю, значит я существую', test_id: tests[2].id },
+  { body: 'Картины Айвазовского', test_id: tests[3].id }
+])
 
-answer = []
-answer << Answer.create!(body:'Сила тока', question_id: question[0].id)
-answer << Answer.create!(body:'1939', question_id: question[1].id)
-answer << Answer.create!(body: 'Рене Декарт', question_id: question[2].id)
-answer << Answer.create!(body: '9 вал', question_id: question[3].id)
-answer << Answer.create!(body: 'Всемиррный потоп', question_id: question[3].id)
+answers = Answer.create!([
+  { body:'Сила тока', question_id: questions[0].id },
+  { body:'1939', question_id: questions[1].id },
+  { body: 'Рене Декарт', question_id: questions[2].id },
+  { body: '9 вал', question_id: questions[3].id },
+  { body: 'Всемиррный потоп', question_id: questions[3].id }
+])
 
-tester = []
-tester << Tester.create!(test_id: test[0].id, user_id: user[2].id)
-tester << Tester.create!(test_id: test[0].id, user_id: user[1].id)
-tester << Tester.create!(test_id: test[1].id, user_id: user[1].id)
-tester << Tester.create!(test_id: test[1].id, user_id: user[2].id)
-tester << Tester.create!(test_id: test[3].id, user_id: user[2].id)
-tester << Tester.create!(test_id: test[2].id, user_id: user[0].id)
+testers = Tester.create!([
+  { test_id: tests[0].id, user_id: users[2].id },
+  { test_id: tests[0].id, user_id: users[1].id },
+  { test_id: tests[1].id, user_id: users[1].id },
+  { test_id: tests[1].id, user_id: users[2].id },
+  { test_id: tests[3].id, user_id: users[2].id },
+  { test_id: tests[2].id, user_id: users[0].id }
+])
