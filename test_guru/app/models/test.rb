9a0410ae1ec0c31889672da047da01df80159711
Kseniya82 <chirkovaksenia82@gmail.com>
2019-01-08 20:07:47 +0300

    Add ActiveRecord associations

--- test_guru/app/models/test.rb
+++ test_guru/app/models/test.rb
@@ -1,4 +1,9 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  has_many :questions
+  has_many :testers
+  has_many :users, through: :testers
+
   def self.select_by_category(title)
     self.joins('JOIN categories ON categories.id = tests.category_id')
     .where(categories: { title: title }).order(title: :desc)
--- test_guru/app/models/user.rb
+++ test_guru/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :testers
+  has_many :tests, through: :testers
+
   def select_all_tests(level)
     Test.joins('JOIN testers ON tests.id = testers.test_id')
     .where(level: level, testers: { user_id: self.id } )
