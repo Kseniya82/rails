a976e616466b6d23e40521ee1f1165f6f733b13f
Kseniya82 <chirkovaksenia82@gmail.com>
2019-01-14 21:20:14 +0300

    Add controller for questions revision 1

--- test_guru/app/controllers/questions_controller.rb
+++ test_guru/app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
-  before_action :find_test, only: :index
+  before_action :find_test, only: [:index, :create]
+  before_action :find_question, only: [:show, :destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
@@ -8,33 +9,36 @@ class QuestionsController < ApplicationController
   end
 
   def show
-    render inline: "<p><%= Question.find(params[:id].to_i).body %></p>"
+    render plain: @question.inspect
   end
 
   def destroy
-    question = Question.find(params[:id].to_i)
-    question.desroy
-    render plain: "Вопрос <%= params[:id].to_i %> удален"
+    @question.destroy
+    render plain: "Вопрос <%= params[:id] %> удален"
   end
 
   def new
   end
 
   def create
-    question = Question.create(question_params)
+    question = @test.questions.create(question_params)
     render plain: question.inspect
   end
 
   private
 
   def find_test
-    @test = Test.find(params[:test_id].to_i)
+    @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question =  Question.find(params[:id])
   end
 
   def question_params
-    params[:test_id] = params[:test_id].to_i
-    params.permit(:test_id, :body)
+    params.require(:question).permit(:body)
   end
+
   def rescue_with_question_not_found
     render plain: 'Вопрос не найден'
   end
