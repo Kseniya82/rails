c19a04181368b051c3314f74e286d46b61757e5f
Kseniya82 <chirkovaksenia82@gmail.com>
2019-01-16 20:04:19 +0300

    Add views and helpers

--- test_guru/app/controllers/questions_controller.rb
+++ test_guru/app/controllers/questions_controller.rb
@@ -1,28 +1,46 @@
 class QuestionsController < ApplicationController
-  before_action :find_test, only: [:index, :create]
-  before_action :find_question, only: [:show, :destroy]
+  before_action :find_test, only: [:index, :create, :new,]
+  before_action :find_question, only: [:show, :destroy, :edit, :update]
+  before_action :find_question_test, only: [:show, :edit, :destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
-    render inline: "<% @test.questions.each do |question| %><p><%= question.body %></p><% end %>"
+    @questions = @test.questions
   end
 
   def show
-    render plain: @question.inspect
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render :edit
+    end
   end
 
   def destroy
     @question.destroy
-    render plain: "Вопрос <%= params[:id] %> удален"
+    redirect_to test_questions_path(@test)
   end
 
   def new
+    @question = @test.questions.new
   end
 
   def create
-    question = @test.questions.create(question_params)
-    render plain: question.inspect
+    @question = @test.questions.new(question_params)
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
   end
 
   private
@@ -33,6 +51,10 @@ class QuestionsController < ApplicationController
 
   def find_question
     @question =  Question.find(params[:id])
+  end
+
+  def find_question_test
+    @test = @question.test
   end
 
   def question_params
--- test_guru/app/helpers/questions_helper.rb
+++ test_guru/app/helpers/questions_helper.rb
@@ -1,2 +1,13 @@
 module QuestionsHelper
+ def question_header
+   if action_name == 'edit'
+     "Edit #{@test.title} question"
+   else
+     "Create New #{@test.title} question"
+   end
+ end
+
+  def github_url(author, repo)
+    "https://github.com/#{author}/#{repo}"
+  end
 end
