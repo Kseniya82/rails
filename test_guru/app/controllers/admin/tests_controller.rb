25b421908b76fe7c6dee1d8ef41d9fb945bf92ac
Kseniya82 <chirkovaksenia82@gmail.com>
2019-01-26 20:24:48 +0300

    Add tests edits only for admins

---
+++ test_guru/app/controllers/admin/tests_controller.rb
@@ -0,0 +1,50 @@
+class Admin::TestsController < Admin::BaseController
+
+  before_action :find_test, only: [:show, :update, :destroy, :edit, :start]
+
+  def index
+    @tests = Test.all
+  end
+
+  def edit; end
+
+  def show
+    @questions = @test.questions
+  end
+
+  def update
+    if @test.update(test_params)
+      redirect_to [:admin, @test]
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @test.destroy
+    redirect_to admin_test_path(@test)
+  end
+
+  def new
+    @test = Test.new
+  end
+
+  def create
+    @test = Test.new(test_params)
+    if @test.save
+      redirect_to [:admin, @test]
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:id])
+  end
+
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id, :user_id)
+  end
+end
