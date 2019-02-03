5e5972a34e11a4da4a291357df0b386a1fbea03c
Kseniya82 <chirkovaksenia82@gmail.com>
2019-02-03 21:20:11 +0300

    Add styles Bootstrap

--- test_guru/app/helpers/application_helper.rb
+++ test_guru/app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
   def current_year
     Time.current.year
   end
+
+  def flash_class(level)
+    case level
+    when :notice then "alert alert-info"
+    when :success then "alert alert-success"
+    when :error then "alert alert-warning"
+    when :alert then "alert alert-warning"
+    end
+  end
 end
