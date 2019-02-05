e5759b29bdf507e271449ca1c374fff7baa10422
Kseniya82 <chirkovaksenia82@gmail.com>
2019-02-05 07:41:13 +0300

    Add styles Bootstrap revision 2

--- test_guru/app/helpers/application_helper.rb
+++ test_guru/app/helpers/application_helper.rb
@@ -4,11 +4,7 @@ module ApplicationHelper
   end
 
   def flash_class(level)
-    case level
-    when "notice" then "alert alert-info"
-    when "success" then "alert alert-success"
-    when "error" then "alert alert-warning"
-    when "alert" then "alert alert-warning"
-    end
+    flash_classes = { notice: 'info', success: 'success', error: 'danger', alert: 'danger' }
+    flash_classes[level.to_sym] || 'primary'
   end
 end
