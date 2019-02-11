e2cc25b99a4c399afeabc9dc164496e1ba86803c
Kseniya82 <chirkovaksenia82@gmail.com>
2019-02-11 09:42:45 +0300

    Add Javascript revision 1

--- test_guru/app/assets/javascripts/devise/registration.js
+++ test_guru/app/assets/javascripts/devise/registration.js
@@ -1,23 +1,23 @@
 document.addEventListener('turbolinks:load', function() {
-  var control = document.getElementById('user_password_confirmation')
-  if (control) { control.addEventListener('input', comparePassword) }
+  var control = document.getElementById('user_password_confirmation');
+  if (control) { control.addEventListener('input', comparePassword); }
 })
 
 function comparePassword() {
-  var password = document.getElementById('user_password').value
-  var password_confirmation = document.getElementById('user_password_confirmation').value
+  var password = document.getElementById('user_password').value;
+  var password_confirmation = document.getElementById('user_password_confirmation').value;
 
   if (password_confirmation.length == 0) {
-    return document.querySelector('.octicon-info').classList.add('hide')
+    return document.querySelector('.octicon-info').classList.add('hide');
   }
 
   if (password_confirmation != password) {
-    document.querySelector('.octicon-info').classList.remove('hide')
-    document.querySelector('.octicon-info').classList.add('text-danger')
-    document.querySelector('.octicon-info').classList.remove('text-success')
+    document.querySelector('.octicon-info').classList.remove('hide');
+    document.querySelector('.octicon-info').classList.add('text-danger');
+    document.querySelector('.octicon-info').classList.remove('text-success');
   } else {
-    document.querySelector('.octicon-info').classList.remove('hide')
-    document.querySelector('.octicon-info').classList.remove('text-danger')
-    document.querySelector('.octicon-info').classList.add('text-success')
+    document.querySelector('.octicon-info').classList.remove('hide');
+    document.querySelector('.octicon-info').classList.remove('text-danger');
+    document.querySelector('.octicon-info').classList.add('text-success');
   }
 }
