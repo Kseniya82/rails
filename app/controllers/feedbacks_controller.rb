f4943dd2710f6be953757edf9967ae7342ff261e
Kseniya82 <chirkovaksenia82@gmail.com>
2019-02-18 11:42:42 +0300

    Application deploy revision 2

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,21 +1,23 @@
 class FeedbacksController < ApplicationController
 
-  def new; end
+  def new
+    @feedback = Feedback.new
+  end
 
   def create
-    if params[:email].present? && params[:message].present?
+    @feedback = Feedback.new(feedback_params)
+    if @feedback.valid?
       FeedbackMailer.sent_feedback(feedback_params).deliver_now
       flash[:notice] = t('.success')
       redirect_to root_path
     else
-      flash[:alert] = t('.failed')
-      redirect_to new_feedback_path
+      render :new
     end
   end
 
   private
 
   def feedback_params
-    params.permit(:email, :message)
+    params.require(:feedback).permit(:email, :message)
   end
 end
